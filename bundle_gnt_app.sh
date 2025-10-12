4b25b3e2818e9bfed7d5faab3bc786bf891c40de
Seth J <152982096+rulingAnts@users.noreply.github.com>
2025-10-12 15:31:46 +0900

    Enhance comments for SBLGNT module bundling
    Added example usage comments for bundling SBLGNT module files.

--- bundle_gnt_app.sh
+++ bundle_gnt_app.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/zsh
 # This command bundles Python, PyWebView, pysword, and your HTML into one file
 pyinstaller --onefile --noconsole --add-data "path/to/your/sword/mods:sword_modules" app.py
+
+
+#or...
+# Example: If SBLGNT module files are in /path/to/SBLGNT/
+#pyinstaller --onefile --noconsole --add-data "/path/to/SBLGNT.conf:mods.d" --add-data "/path/to/SBLGNT.bbl:modules/texts" app.py
